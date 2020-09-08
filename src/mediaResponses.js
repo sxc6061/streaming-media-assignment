12503128d0912648ca3188ffd5aaaa464506bd99
Steven Cho <sxc6061@g.rit.edu>
2020-09-08 16:58:07 -0400

    completed assignment

---
+++ src/mediaResponses.js
@@ -0,0 +1,70 @@
+const fs = require('fs');
+const path = require('path');
+const { dir } = require('console');
+
+const loadFile = (request, response,contentType,fileDir) => {
+  const file = path.resolve(__dirname, fileDir);
+
+  fs.stat(file, (err, stats) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        response.writeHead(404);
+      }
+      return response.end(err);
+    }
+
+    let { range } = request.headers;
+
+    if (!range) {
+      range = 'bytes=0-';
+    }
+
+    const positions = range.replace(/bytes=/, '').split('-');
+
+    let start = parseInt(positions[0], 10);
+
+    const total = stats.size;
+    const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+
+    if (start > end) {
+      start = end - 1;
+    }
+
+    const chunksize = (end - start) + 1;
+
+    response.writeHead(206, {
+      'Content-Range': `bytes ${start}-${end}/${total}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': contentType,
+    });
+
+    const stream = fs.createReadStream(file, { start, end });
+
+    stream.on('open', () => {
+      stream.pipe(response);
+    });
+
+    stream.on('error', (streamErr) => {
+      response.end(streamErr);
+    });
+
+    return stream;
+  });
+};
+
+const getParty = (request, response) => {
+    loadFile(request, response,'video/mp4','../client/party.mp4');
+};
+
+const getBling = (request, response) => {
+  loadFile(request, response, 'audio/mpeg','../client/bling.mp3');
+};
+
+const getBird = (request, response) => {
+  loadFile(request, response, 'video/mp4','../client/bird.mp4');
+};
+
+module.exports.getParty = getParty;
+module.exports.getBling = getBling;
+module.exports.getBird = getBird;
